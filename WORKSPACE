2ca96c9138f12af98917e24082ac33d8b63ea637
Jing Liu <python.leojay@gmail.com>
2021-02-08 22:40:20 -0800

    1. Add boringssl. 2. Add tcmalloc runtime.

--- WORKSPACE
+++ WORKSPACE
@@ -7,10 +7,17 @@ ALL_CONTENT = 'filegroup(name = "all", srcs = glob(["**"]), visibility = ["//vis
 ROOT_3P = 'file://' + __workspace_dir__ + '/3p/'
 
 http_archive(
-    name = 'absl',
+    # Always use the same name as in the WORKSPACE file of the external project. i.e. com_google_absl instead of absl.
+    name = 'com_google_absl',
     url = ROOT_3P + 'abseil-cpp-master.tar.xz',
     strip_prefix = 'abseil-cpp-master',
     sha256 = '2efbd20528623dbce8eb5aeb3f91f037259034c95a972b000e7f051dedc63d05',
+)
+
+http_archive(
+    name = 'com_google_tcmalloc',
+    url = ROOT_3P + 'tcmalloc-master.tar.xz',
+    strip_prefix = 'tcmalloc-master',
 )
 
 http_archive(
@@ -19,6 +26,12 @@ http_archive(
     url = ROOT_3P + 'libmicrohttpd-0.9.72.tar.xz',
     strip_prefix = 'libmicrohttpd-0.9.72',
     sha256 = '4eb9e02c7e91b4f630d08cf7b4443ca2286f78f7e4390c8ba8c0d715f69deffa',
+)
+
+http_archive(
+    name = 'boringssl',
+    url = ROOT_3P + 'chromium-stable-with-bazel.tar.xz',
+    sha256 = 'f3055d4f0fa72275b8c5fd5561c8e6b7546b49a19f7b248bc92f9584ccdd62eb',
 )
 
 http_archive(
--- src/hello_world.cc
+++ src/hello_world.cc
@@ -2,38 +2,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <microhttpd.h>
 #include "absl/debugging/failure_signal_handler.h"
 #include "absl/debugging/symbolize.h"
+#include "absl/strings/escaping.h"
+#include "absl/strings/match.h"
+#include "absl/strings/numbers.h"
+#include "absl/strings/string_view.h"
+#include "microhttpd.h"
+#include "openssl/digest.h"
 
 #define PAGE \
   "<html><head><title>libmicrohttpd demo</title></head><body>Query string for &quot;%s&quot; was &quot;%s&quot;</body></html>"
 
+namespace {
+MHD_Response* g_empty_response;
+const absl::string_view kHttpMethodGet = MHD_HTTP_METHOD_GET;
+
+void init_global_responses() {
+    g_empty_response = MHD_create_response_from_buffer(0, (void*)"", MHD_RESPMEM_PERSISTENT);
+}
+
+MHD_Result method_not_allowed(MHD_Connection* connection) {
+    return MHD_queue_response(connection, MHD_HTTP_METHOD_NOT_ALLOWED, g_empty_response);
+}
+
+MHD_Result not_found(MHD_Connection* connection) {
+    return MHD_queue_response(connection, MHD_HTTP_NOT_FOUND, g_empty_response);
+}
+}
+
 static enum MHD_Result
 ahc_echo(void* cls,
-         struct MHD_Connection* connection,
+         MHD_Connection* connection,
          const char* url,
          const char* method,
          const char* version,
          const char* upload_data, size_t* upload_data_size, void** ptr) {
-    static int aptr;
+    static int dummy_ptr;
     const char* fmt = (const char*) cls;
     const char* val;
     char* me;
-    struct MHD_Response* response;
+    MHD_Response* response;
     enum MHD_Result ret;
     (void) url;               /* Unused. Silent compiler warning. */
     (void) version;           /* Unused. Silent compiler warning. */
     (void) upload_data;       /* Unused. Silent compiler warning. */
     (void) upload_data_size;  /* Unused. Silent compiler warning. */
 
-    if (strcmp(method, MHD_HTTP_METHOD_GET))
-        return MHD_NO;              /* unexpected method */
-    if (&aptr != *ptr) {
+    if (method != kHttpMethodGet) {
+        return method_not_allowed(connection);
+    }
+    if (&dummy_ptr != *ptr) {
         /* do never respond on first call */
-        *ptr = &aptr;
+        *ptr = &dummy_ptr;
         return MHD_YES;
     }
+
+    if (absl::EndsWithIgnoreCase(url, ".ico")) {
+        return not_found(connection);
+    }
+
     *ptr = NULL;                  /* reset when done */
     val = MHD_lookup_connection_value(connection, MHD_GET_ARGUMENT_KIND, "q");
     me = (char*) malloc(snprintf(NULL, 0, fmt, "q", val) + 1);
@@ -51,25 +79,41 @@ ahc_echo(void* cls,
     return ret;
 }
 
+void boringssl_sha256_demo() {
+    std::string s = "hello world!";
+    uint8_t digest[EVP_MAX_MD_SIZE];
+    const EVP_MD *hash = EVP_sha256();
+    unsigned int digest_len = EVP_MAX_MD_SIZE;
+    if (EVP_Digest(s.data(), s.length(), digest, &digest_len, hash, nullptr) != 0) {
+        std::string hex_digest = absl::BytesToHexString(absl::string_view((const char*) digest, digest_len));
+        printf("Digest is: %s\n", hex_digest.c_str());
+    } else {
+        printf("Failed to calculate digest.\n");
+    }
+}
+
 
 int main(int argc, char* const* argv) {
-    struct MHD_Daemon* d;
-    int port;
-
     absl::InitializeSymbolizer(argv[0]);
     absl::InstallFailureSignalHandler({});
+
+    boringssl_sha256_demo();
 
     if (argc != 2) {
         printf("%s PORT\n", argv[0]);
         return 1;
     }
-    port = atoi(argv[1]);
-    if ((port < 0) ||
-            (port > UINT16_MAX)) {
+
+    init_global_responses();
+
+    int port = 0;
+    if (!absl::SimpleAtoi(argv[1], &port) || port < 0 || port > UINT16_MAX) {
         printf("%s PORT\n", argv[0]);
         return 1;
     }
-    d = MHD_start_daemon(MHD_USE_EPOLL | MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_ERROR_LOG,
+
+    MHD_Daemon* d;
+    d = MHD_start_daemon(MHD_USE_EPOLL | MHD_USE_INTERNAL_POLLING_THREAD,
                          (uint16_t) port,
                          NULL, NULL, &ahc_echo, (void*) PAGE,
                          MHD_OPTION_CONNECTION_TIMEOUT, (unsigned int) (120 /* seconds */),
@@ -82,4 +126,3 @@ int main(int argc, char* const* argv) {
     MHD_stop_daemon(d);
     return 0;
 }
-
